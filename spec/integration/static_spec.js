675e0c872fe9d8a20c9a557e77607e668d3a12a4
joshuadconrad <joshuadconrad@gmail.com>
2019-07-27 14:59:28 -0500

    Assignment-2982: Submission

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,31 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const base = "http://localhost:3000";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
 
-//#2
     it("should return status code 200", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
+        expect(res.body).toContain("Welcome to Bloccit");
         done();
       });
     });
 
   });
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200 and the body should contain the string 'polo'", (done) => {
+      request.get(base + "/marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toContain("polo");
+        done();
+      });
+    });
+
+  });
+
 });
