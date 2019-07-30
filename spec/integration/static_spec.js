808866c34544c42bc176aa28f3e2cb6a4ed2018c
joshuadconrad <joshuadconrad@gmail.com>
2019-07-29 21:30:16 -0500

    Assignment-2995: Submission

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,10 +1,11 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const base = "http://localhost:3000";
 
 describe("routes : static", () => {
 
   describe("GET /", () => {
+
     it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
@@ -12,6 +13,19 @@ describe("routes : static", () => {
         done();
       });
     });
+
   });
-  
+
+  describe("GET /about", () => {
+
+    it("should return status code 200 and have 'About' in the body of the response", (done) => {
+      request.get(base + "/about", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About");
+        done();
+      });
+    });
+
+  });
+
 });
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -3,5 +3,12 @@ module.exports = {
     res.render("static/index", {
       title: "Welcome to Bloccit"
     });
+  },
+
+  about(req, res, next) {
+    res.render("static/about", {
+      title: "About"
+    });
   }
+
 }
