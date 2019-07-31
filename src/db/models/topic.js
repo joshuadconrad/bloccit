45b436f003e3a2b821659ad0df2b9e7593c7e157
joshuadconrad <joshuadconrad@gmail.com>
2019-07-31 18:41:00 -0500

    Checkpoint-2997: Slack Help

---
+++ src/db/models/topic.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Topic = sequelize.define('Topic', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  Topic.associate = function(models) {
+    // associations can be defined here
+    Topic.hasMany(models.Banner, {
+     foreignKey: "topicsId",
+     as: "Banners",
+   });
+  };
+  return Topic;
+};
