c1089254848a831703c0ef1fcfa93f0ac3bf0114
joshuadconrad <joshuadconrad@gmail.com>
2019-08-03 12:31:51 -0500

    Checkpoint-3003: Complete

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/topics";
+const base = "http://localhost:3000/topics/";
 
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
@@ -8,19 +8,21 @@ const Topic = require("../../src/db/models").Topic;
 describe("routes : topics", () => {
   beforeEach(done => {
     this.topic;
-    sequelize.sync({force: true}).then(response => {
+    sequelize.sync({
+      force: true
+    }).then(response => {
       Topic.create({
-        title: "JS Frameworks",
-        description: "There is a lot of them"
-      })
-      .then(topic => {
-        this.topic = topic;
-        done();
-      })
-      .catch(error => {
-        console.log(error);
-        done();
-      });
+          title: "JS Frameworks",
+          description: "There is a lot of them"
+        })
+        .then(topic => {
+          this.topic = topic;
+          done();
+        })
+        .catch(error => {
+          console.log(error);
+          done();
+        });
     });
   });
 
@@ -35,4 +37,93 @@ describe("routes : topics", () => {
       });
     });
   });
+
+  describe("GET /topics/new", () => {
+
+    it("should render a new topic form", (done) => {
+      request.get(`${base}new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Topic");
+        done();
+      });
+    });
+  });
+
+  describe("GET /topics/:id", () => {
+
+     it("should render a view with the selected topic", (done) => {
+       request.get(`${base}${this.topic.id}`, (err, res, body) => {
+         expect(err).toBeNull();
+         expect(body).toContain("JS Frameworks");
+         done();
+       });
+     });
+
+   });
+
+   describe("POST /topics/:id/destroy", () => {
+
+     it("should delete the topic with the associated ID", (done) => {
+
+       Topic.all()
+
+       .then((topics) => {
+         const topicCountBeforeDelete = topics.length;
+         expect(topicCountBeforeDelete).toBe(1);
+         request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+           Topic.all()
+           .then((topics) => {
+             expect(err).toBeNull();
+             expect(topics.length).toBe(topicCountBeforeDelete - 1);
+             done();
+           })
+         });
+       });
+
+     });
+
+   });
+
+   describe("GET /topics/:id/edit", () => {
+
+     it("should render a view with an edit topic form", (done) => {
+       request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+         expect(err).toBeNull();
+         expect(body).toContain("Edit Topic");
+         expect(body).toContain("JS Frameworks");
+         done();
+       });
+     });
+
+   });
+
+   describe("POST /topics/:id/update", () => {
+
+    it("should update the topic with the given values", (done) => {
+       const options = {
+          url: `${base}${this.topic.id}/update`,
+          form: {
+            title: "JavaScript Frameworks",
+            description: "There are a lot of them"
+          }
+        };
+
+        request.post(options,
+          (err, res, body) => {
+
+          expect(err).toBeNull();
+
+          Topic.findOne({
+            where: { id: this.topic.id }
+          })
+          .then((topic) => {
+            expect(topic.title).toBe("JavaScript Frameworks");
+            done();
+          });
+        });
+    });
+
+  });
+
+
 });
